654efd0454cbc04c8b02c955c9a303503422b5f0
Sachin <chavansachin5030@gmail.com>
2026-04-29 09:29:47 +0530

    pg update

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,7 +3,7 @@ import cors from "cors"
 import authRouts from './routes/auth.routes'
 import adminRouts from './routes/admin.routes'
 import employeeRouts from './routes/employee.routes'
-import { FRONTEND_URL } from "./config/env"
+import { FRONTEND_URL, NODE_ENV, PRODUCTION } from "./config/env"
 import cookieParser from "cookie-parser"
 import { adminProtect, protect } from "./moddlewares/auth.middleware"
 const app = express()
@@ -24,6 +24,12 @@ app.get("/", (req, res) => {
     res.json({ message: "API running successfully" })
 })
 const PORT = 5000
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+
+if (NODE_ENV !== PRODUCTION) {
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+    })
+}
+
+export default app
